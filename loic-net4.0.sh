bf96719a999be97315abe54e3f69cfc43e8ab120
cyberanand1337x <114714715+cyberanand1337x@users.noreply.github.com>
2023-11-13 16:30:53 +0530

    Update loic-net4.0.sh

--- loic-net4.0.sh
+++ loic-net4.0.sh
@@ -15,7 +15,7 @@
 # Usage: bash ./loic-net4.0.sh <install|update|run>
 #
 
-GIT_REPO=https://github.com/cyberanand1337x/Low-Orbit-Ion-Cannon-2023.git
+GIT_REPO=https://github.com/cyberanand1337x/Loic.git
 GIT_BRANCH=master
 
 DEB_MONO_PKG="monodevelop liblog4net-cil-dev mono-devel mono-runtime-common mono-runtime libmono-system-windows-forms4.0-cil"
